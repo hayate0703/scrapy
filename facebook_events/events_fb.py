16c55c5b3e787f63a2d6665d4c7ff02d4d3a4fa0
Hung Nguyen Viet <hvn@familug.org>
2016-04-23 11:35:32 +0700

    pep8

--- facebook_events/events_fb.py
+++ facebook_events/events_fb.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import argparse
 import time
 import json
 import logging
@@ -8,9 +7,9 @@ import os
 import requests
 from coorcal import generate_coordinate
 
-#parser = argparse.ArgumentParser(description='Export all events of a location with Facebook Graph API')
 logging.basicConfig(
-    filename="debug.log", level=logging.DEBUG, format='%(levelname)s:%(message)s')
+    filename="debug.log",
+    level=logging.DEBUG, format='%(levelname)s:%(message)s')
 
 # Load config from file config.json
 with open('config.json', 'r') as f:
@@ -24,12 +23,17 @@ token = requests.get("https://graph.facebook.com/v2.5/oauth/access_token?client_
 if not os.path.exists("./results"):
     os.makedirs("./results")
 
+
 def get_page_id(lat, lon):
     '''
     Get pages's ID from a location. Return a list of all ID.
     '''
     pages_id = requests.get("https://graph.facebook.com/v2.5/search?type=place&q={0}&center={1},{2}&distance={3}&limit={4}&fields=id&access_token={5}".format(
-        args["keyword"], lat, lon, args["distance"], args["limit"], token.content.split('"')[3]))
+        args["keyword"],
+        lat, lon,
+        args["distance"],
+        args["limit"],
+        token.content.split('"')[3]))
     # Create a list of all ID
     pages_id_list = [i.values()[0] for i in pages_id.json()['data']]
     return pages_id_list
@@ -37,7 +41,8 @@ def get_page_id(lat, lon):
 
 def get_envents_from_page_id(id):
     '''
-    For each page ID, find all event (if have) of that Page from today. Return a dictionary of page's infos and it's events.
+    For each page ID, find all event (if have) of that Page from today.
+    Return a dictionary of page's infos and it's events.
     '''
     events = requests.get("https://graph.facebook.com/v2.5/?ids={0}&fields=events.fields(id,name,start_time,description,place,type,category,ticket_uri,cover.fields(id,source),picture.type(large),attending_count,declined_count,maybe_count,noreply_count).since({1}),id,name,cover.fields(id,source),picture.type(large),location&access_token={2}".format(
         id, time.strftime("%Y-%m-%d"), token.content.split('"')[3]))
@@ -47,12 +52,9 @@ if __name__ == '__main__':
     CIRCLE = (21.027875, 105.853654, 10000,)
     for point in generate_coordinate(*CIRCLE, scan_radius=args["distance"]):
         for i in get_page_id(point[0], point[1]):
-            if get_envents_from_page_id(i).values()[0].has_key('events'):
+            if 'events' in get_envents_from_page_id(i).values()[0]:
                 file_name = "./results/" + i + ".json"
                 with open(file_name, 'wb') as f:
                     json.dump(get_envents_from_page_id(i), f, indent=4)
             else:
                 pass
-
-
-
